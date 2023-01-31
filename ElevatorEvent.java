3a58144538051e69fcc16cc142f9f8ab30e61af9
ajayitomisin <82481467+ajayitomisin@users.noreply.github.com>
2023-01-30 20:57:27 -0500

    Update ElevatorEvent.java

--- ElevatorEvent.java
+++ ElevatorEvent.java
@@ -1,3 +1,13 @@
+/**
+ * This record encapsulates an event occurring in the ElevatorSystem.
+ *
+ * @author Oluwatomisin Ajayi
+ * @vesrsion January 30th, 2023
+ *
+ * @param time the time it takes for the simulation to occur.
+ * @param currentFloor the current floor of the elevator in the simulation.
+ * @param button the number of the next floor the elevator goes to.
+ */
 public record ElevatorEvent(int time, int currentFloor, int button) {
     
 }
