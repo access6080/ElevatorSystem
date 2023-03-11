d2e0c4dedee562b98fe32ab5a6bc10c0340ebfb1
BomaIyaye <113210994+BomaIyaye@users.noreply.github.com>
2023-03-10 21:58:14 -0500

    Add files via upload

---
+++ Lamp.java
@@ -0,0 +1,37 @@
+public class Lamp {
+    /**
+     * This class models a lamp object
+     * @author Boma Iyaye
+     * @version
+     */
+    boolean lampState;
+
+    /**
+     * Creates a lamp object that is turned off
+     */
+    public Lamp() {
+        this.lampState = false;
+    }
+    /**
+     * Turns the lamp on by setting the lamp state to true
+     */
+    public void turnOn(){
+        lampState = true;
+    }
+
+    /**
+     * Turns the lamp off by setting the lamp state to false
+     */
+    public void turnOff(){
+        lampState = false;
+    }
+
+    /**
+     * Returns the current state of the lamp.
+     * @return  true if turned on ,return false if turned off
+     */
+    public boolean isLampState() {
+        return lampState;
+    }
+}
+
