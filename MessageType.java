078eff3b0af38d8cd1032b58a74b93fb062dc5c9
ajayitomisin <82481467+ajayitomisin@users.noreply.github.com>
2023-03-15 10:34:36 -0400

    Update MessageType.java

--- MessageType.java
+++ MessageType.java
@@ -1,6 +1,6 @@
 /**
- *This class is an enum class containing the different message types
- *@author Geoffery Koranteng
+* This class is an enum class containing the different message types
+* @author Geoffery Koranteng
 public enum MessageType {
         Job,
         Function_Request,
