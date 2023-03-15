b270f354683188a898d2b0f54581a7d5f68d1bbc
chibuzookpara <114440675+chibuzookpara@users.noreply.github.com>
2023-03-15 16:40:43 -0400

    motor, door and lamp tests

---
+++ DoorTest.java
@@ -0,0 +1,60 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * this class tests the door class
+ * @author chibuzo okpara
+ * @version 15th of March, 2023
+ */
+public class DoorTest {
+    private Door door;
+
+    @Before
+    public void setup(){
+
+        door = new Door();
+    }
+
+    @After
+    public void tearDown(){}
+
+    @Test
+    public void GetInitialStatusTest() {
+        assertEquals(Door.DoorState.CLOSED, door.getStatus());
+    }
+
+    @Test
+    public void openDoorTest(){
+        door.openDoor();
+        assertEquals(Door.DoorState.OPEN, door.getStatus());
+    }
+
+    @Test
+    public void movingDoorTest(){
+        door.doorMoving();
+        assertEquals(Door.DoorState.MOVING, door.getStatus());
+        }
+
+    @Test
+    public void closedWhileMovingDoorTest(){
+        door.doorMoving();
+        door.openDoor();
+        assertNotEquals(Door.DoorState.OPEN, door.getStatus());
+        }
+    @Test
+    public void closedDoorTest(){
+        door.openDoor();
+        door.closeDoor();
+        assertEquals(Door.DoorState.OPEN, door.getStatus());
+    }
+
+    }
+
+
+
+
+
